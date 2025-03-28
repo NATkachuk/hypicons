621fa4fe04577c1913d505c4ad11e6ab9f5b5f55
NATkachuk <nazartkacuk301@gmail.com>
2025-03-28 15:58:12 +0300

    Section4 done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import "./Main.scss"
 import Section1 from "./Section1/Section1"
 import Section2 from "./Section2/Section2"
 import Section3 from "./Section3/Section3"
+import Section4 from "./Section4/Section4"
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
         <Section1/>
         <Section2/>
         <Section3/>
+        <Section4/>
     </main>
   )
 }
---
+++ src/components/Main/Section4/Section4.jsx
@@ -0,0 +1,130 @@
+import "./Section4.scss"
+import Apps from "../../../assets/Homepage/png/Apps.png"
+function Section4() {
+  return (
+    <section className="section4">
+        <div className="section4-container1">
+            <h2 className="section4-container1__title">Big Sur ready</h2>
+            <img src={Apps} alt="Apps" className="section4-container1__img" />
+        </div>
+        <div className="section4-container2">
+            <ul className="section4-container2__list">
+                <p className="section4-container2__list-title">Health & Fitness</p>
+                <li className="section4-container2__item">Adidas</li>
+                <li className="section4-container2__item">Activity</li>
+                <li className="section4-container2__item">Covid-19</li>
+                <li className="section4-container2__item">Fitbit</li>
+                <li className="section4-container2__item">Nike</li>
+                <li className="section4-container2__item">Peloton</li>
+                <li className="section4-container2__item">Sleep</li>
+                <li className="section4-container2__item">Strava</li>
+                <li className="section4-container2__item">MyFitnessPal</li>
+                <li className="section4-container2__item">Weights</li>
+            </ul>
+            <ul className="section4-container2__list">
+                <p className="section4-container2__list-title">Social Media</p>
+                <li className="section4-container2__item">Behance</li>
+                <li className="section4-container2__item">Bumble</li>
+                <li className="section4-container2__item">Discord</li>
+                <li className="section4-container2__item">Dribbble</li>
+                <li className="section4-container2__item">Facebook</li>
+                <li className="section4-container2__item">Flickr</li>
+                <li className="section4-container2__item">Google Hangouts</li>
+                <li className="section4-container2__item">Instagram</li>
+                <li className="section4-container2__item">Linkedin</li>
+                <li className="section4-container2__item">Meetup</li>
+                <li className="section4-container2__item">Messenger</li>
+                <li className="section4-container2__item">Pinterest</li>
+                <li className="section4-container2__item">Quora</li>
+                <li className="section4-container2__item">Reddit</li>
+                <li className="section4-container2__item">Skype</li>
+                <li className="section4-container2__item">Slack</li>
+                <li className="section4-container2__item">Snapchat</li>
+                <li className="section4-container2__item">Teams</li>
+                <li className="section4-container2__item">Telegram</li>
+                <li className="section4-container2__item">TikTok</li>
+                <li className="section4-container2__item">Tinder</li>
+                <li className="section4-container2__item">Tumblr</li>
+                <li className="section4-container2__item">Twitch</li>
+                <li className="section4-container2__item">Twitter</li>
+                <li className="section4-container2__item">Vimeo</li>
+                <li className="section4-container2__item">VK</li>
+                <li className="section4-container2__item">Whatsapp</li>
+                <li className="section4-container2__item">Youtube</li>
+                <li className="section4-container2__item">Zoom</li>
+            </ul>
+            <ul className="section4-container2__list">
+                <p className="section4-container2__list-title">General</p>
+                <li className="section4-container2__item">Airbnb</li>
+                <li className="section4-container2__item">Cash App</li>
+                <li className="section4-container2__item">Chrome</li>
+                <li className="section4-container2__item">Deliveroo</li>
+                <li className="section4-container2__item">Dropbox</li>
+                <li className="section4-container2__item">Ebay</li>
+                <li className="section4-container2__item">Figma</li>
+                <li className="section4-container2__item">Github</li>
+                <li className="section4-container2__item">Gmail</li>
+                <li className="section4-container2__item">Google Drive</li>
+                <li className="section4-container2__item">Google Home</li>
+                <li className="section4-container2__item">Gumtree</li>
+                <li className="section4-container2__item">Invision</li>
+                <li className="section4-container2__item">Lyft</li>
+                <li className="section4-container2__item">Monzo</li>
+                <li className="section4-container2__item">Notion</li>
+                <li className="section4-container2__item">Paypal</li>
+                <li className="section4-container2__item">Uber</li>
+                <li className="section4-container2__item">VSCO</li>
+            </ul>
+            <ul className="section4-container2__list">
+                <p className="section4-container2__list-title">Entertainment</p>
+                <li className="section4-container2__item">Alexa</li>
+                <li className="section4-container2__item">Amazon</li>
+                <li className="section4-container2__item">Apple (TV)</li>
+                <li className="section4-container2__item">Channel 4</li>
+                <li className="section4-container2__item">Crunchyroll</li>
+                <li className="section4-container2__item">Disney Plus</li>
+                <li className="section4-container2__item">Google Photos</li>
+                <li className="section4-container2__item">Google Play</li>
+                <li className="section4-container2__item">Hulu</li>
+                <li className="section4-container2__item">Netflix</li>
+                <li className="section4-container2__item">Oculus</li>
+                <li className="section4-container2__item">Playstation</li>
+                <li className="section4-container2__item">Shazam</li>
+                <li className="section4-container2__item">Sky</li>
+                <li className="section4-container2__item">Soundcloud</li>
+                <li className="section4-container2__item">Spotify</li>
+            </ul>
+            <ul className="section4-container2__list">
+            <p className="section4-container2__list-title">Apple Utilities</p>
+                <li className="section4-container2__item">Calculator</li>
+                <li className="section4-container2__item">Calendar</li>
+                <li className="section4-container2__item">Camera</li>
+                <li className="section4-container2__item">Clock</li>
+                <li className="section4-container2__item">Contacts</li>
+                <li className="section4-container2__item">Facetime</li>
+                <li className="section4-container2__item">Find my phone</li>
+                <li className="section4-container2__item">Flashlight</li>
+                <li className="section4-container2__item">Health</li>
+                <li className="section4-container2__item">iMovie</li>
+                <li className="section4-container2__item">Keynote</li>
+                <li className="section4-container2__item">Mail</li>
+                <li className="section4-container2__item">Maps</li>
+                <li className="section4-container2__item">Messages</li>
+                <li className="section4-container2__item">Music</li>
+                <li className="section4-container2__item">Numbers</li>
+                <li className="section4-container2__item">Pages</li>
+                <li className="section4-container2__item">Phone</li>
+                <li className="section4-container2__item">Photos</li>
+                <li className="section4-container2__item">Podcasts</li>
+                <li className="section4-container2__item">Remote</li>
+                <li className="section4-container2__item">Safari</li>
+                <li className="section4-container2__item">Shortcuts</li>
+                <li className="section4-container2__item">Stopwatch</li>
+                <li className="section4-container2__item">Weather</li>
+            </ul>
+        </div>
+    </section>
+  )
+}
+
+export default Section4
