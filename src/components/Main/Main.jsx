7863e719cbcb1f6df9ede244acdfce96cd925169
NATkachuk <nazartkacuk301@gmail.com>
2025-03-31 15:46:06 +0300

    section6 done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,7 @@ import Section2 from "./Section2/Section2"
 import Section3 from "./Section3/Section3"
 import Section4 from "./Section4/Section4"
 import Section5 from "./Section5/Section5"
+import Section6 from "./Section6/Section6"
 
 function Main() {
   return (
@@ -13,6 +14,7 @@ function Main() {
         <Section3/>
         <Section4/>
         <Section5/>
+        <Section6/>
     </main>
   )
 }
---
+++ src/components/Main/Section6/Section6.jsx
@@ -0,0 +1,17 @@
+import "./Section6.scss"
+
+function Section6() {
+  return (
+    <section className="section6">
+        <div className="section6-box">
+            <div className="section6-box__textbox">
+                <h2 className="section6-box__textbox-title">Grab the pack</h2>
+                <p className="section6-box__textbox-text">By purchasing the pack you will get access to future additions and updates – for life!</p>
+            </div>
+            <button className="section6-box__btn">Buy now - $14.99</button>
+        </div>
+    </section>
+  )
+}
+
+export default Section6
